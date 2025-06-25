20d6a33138d76352c8cbb742defb876cc2888f56
eli_shteinman <a0533057932@gmail.com>
2025-06-25 09:19:37 +0300

    Add `Square` class extending `Rectangle`

---
+++ square.py
@@ -0,0 +1,8 @@
+from rectangle import Rectangle
+
+class Square(Rectangle):
+    def __init__(self, side_length):
+        super().__init__(side_length, side_length)
+
+    def __str__(self):
+        return f"Square with side length {self.width}"
