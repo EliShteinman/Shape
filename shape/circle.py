c73b825c648b438f71a74d12405d6a6d3c91a61a
eli_shteinman <a0533057932@gmail.com>
2025-06-25 09:50:02 +0300

    Add `Circle` class extending `Shape` with type annotations and docstrings

---
+++ shape/circle.py
@@ -0,0 +1,28 @@
+from math import pi
+from shape import Shape
+
+class Circle(Shape):
+    """
+    Circle class that inherits from Shape.
+    """
+
+    def __init__(self, radius: float) -> None:
+        """
+        Initialize a Circle instance.
+        :param radius:
+        """
+        self.radius:float = radius
+
+    def get_area(self) -> float:
+        """
+        Calculate the area of the circle.
+        :return: float - The area of the circle.
+        """
+        return pi * self.radius ** 2
+
+    def __str__(self) -> str:
+        """
+        Return a string representation of the circle.
+        :return: str - String representation of the circle.
+        """
+        return f"Circle with radius {self.radius}"
